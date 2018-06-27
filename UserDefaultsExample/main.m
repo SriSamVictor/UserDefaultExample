02cf8ff8dc142c1ed1ddd7f521114161e5009e02
Srinivasan Sampath <sri.persnl@gmail.com>
2018-06-27 18:42:10 +0530

    Update main.m

--- UserDefaultsExample/main.m
+++ UserDefaultsExample/main.m
@@ -12,6 +12,12 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        UserDefaultsSample *userDefaultSample = [[UserDefaultsSample alloc]init];
+        [userDefaultSample defaultsDisp:001 firstName:@"Srinivasan" secondName:@"Sampath"];
+        
+        UserDefaultsDemo *userDefaultDemo = [[UserDefaultsDemo alloc]init];
+        [userDefaultDemo Sample];
     }
     return 0;
 }
