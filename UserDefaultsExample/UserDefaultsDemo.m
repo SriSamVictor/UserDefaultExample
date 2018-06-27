5c071314892b2a8b56a25976fe946c47103fb634
Srinivasan Sampath <sri.persnl@gmail.com>
2018-06-27 18:39:30 +0530

    Update UserDefaultsDemo.m

--- UserDefaultsExample/UserDefaultsDemo.m
+++ UserDefaultsExample/UserDefaultsDemo.m
@@ -10,4 +10,42 @@
 
 @implementation UserDefaultsDemo
 
+@synthesize dict;
+
+-(void)defaultsDisp:(NSInteger)sum firstName:(NSString *)firstName secondName:(NSString *)secondName{
+    
+   self.arr = [NSMutableArray arrayWithObjects:@"Paris",@"Japan",@"Asia",@"America",@"India",@"German",@"Spain", nil];
+    
+    dict = [NSMutableDictionary dictionaryWithObjects:@[@"A",@"B",@"C"] forKeys:@[@"key1",@"key2",@"key3"]];
+
+// NSUserDefaults standaraddefaults
+    NSUserDefaults *userDefaults1 = [NSUserDefaults standardUserDefaults];
+    
+// Set Integer for key
+    [userDefaults1 setInteger:9001 forKey:@"High Score"];
+    
+// Set Float for key
+    [userDefaults1 setFloat:45.33 forKey:@"floatValue"];
+    
+// Set Double for key
+    [userDefaults1 setDouble:4500089 forKey:@"doubleValue"];
+    
+// Set Object for key
+    [userDefaults1 setObject:@[@"Brinjal",@"Tomato",@"Potato"] forKey:@"sampleArray"];
+    
+// Set Bool for key
+    [userDefaults1 setBool:YES forKey:@"IOS"];
+    
+// Set Object for key
+    [userDefaults1 setObject:@{@"User" : @"Srini"} forKey:@"dictKey"];
+    
+// Set Object for key
+    [userDefaults1 setObject:self.arr forKey:@"newArr"];
+    
+// Set url for key
+    [userDefaults1 setURL:[NSURL URLWithString:@"www.google.co.in"] forKey:@"website"];
+    
+ // Set Value for key
+    [userDefaults1 setValue:dict forKey:@"dictKey"];  
+  
 @end
