995e71bc32d2f8f4a10eabc529e0993538a8f4d3
Sri Sampath <sri.persnl@gmail.com>
2018-06-27 10:36:22 +0530

    Initial Commit

---
+++ UserDefaultsExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UserDefaultsExample
+//
+//  Created by Jean Martin on 27/06/18.
+//  Copyright © 2018 Public. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
